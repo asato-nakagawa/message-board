3a86a5a3226a5eb648a8e94dbcd86bf852f19488
asato.nakagawa <16h5092@stu.meisei-u.ac.jp>
2019-08-10 08:16:32 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,11 @@
 class MessagesController < ApplicationController
+  berore_action :set_message, only: [:show, :edit, :update, :destroy]
 
   def index
     @messages = Message.all
   end
 
   def show
-    @message = Message.find(params[:id])
   end
 
   def new
@@ -25,12 +25,9 @@ class MessagesController < ApplicationController
   end
 
   def edit
-    @message = Message.find(params[:id])
   end
 
   def update
-    @message = Message.find(params[:id])
-    
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
       redirect_to @message
@@ -42,14 +39,16 @@ class MessagesController < ApplicationController
   end
 
   def destroy
-    @message = Message.find(params[:id])
     @message.destroy
     
     flash[:success] = 'Message は正常に削除されました'
-    redirect_to messages_rul
+    redirect_to messages_url
   end
   
   private
+  def set_message
+    @message = Message.find(params[:id])
+  end
   
   # Strong Parameter
   def message_params
